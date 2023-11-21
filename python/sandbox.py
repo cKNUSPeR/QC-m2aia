7492c0631d1795ff93990abf938ed5c823aa5c05
Jannik <jw1217@gmx.de>
2023-11-21 19:31:08 +0100

    commit before adding nectar-msi

--- python/sandbox.py
+++ python/sandbox.py
@@ -1,6 +1,7 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import numpy as np
+import nectar_msi as nc
 
 import m2aia as m2
 def bgin_of_bin():
@@ -30,12 +31,38 @@ def bgin_of_bin():
 
     print(full_df['mz_bins'].to_numpy(), full_df["collect"].to_numpy())
 
-from region_utils import average_processed_spectra, average_cont_spectra
+#from region_utils import average_processed_spectra, average_cont_spectra
+
+from cal_utils import find_nearest_loc_max
+
+
 
 file_name = r"C:\Users\Jannik\Documents\Uni\Master_Biochem\4_Semester\M2aia\data\Example_Continuous_imzML\Example_Continuous.imzML"
 I = m2.ImzMLReader(file_name)
 
-mzs, ints = average_processed_spectra(I, [2,3,4])
+mzs, ints = I.GetSpectrum(6)
 
-print(mzs)
-print(ints)
+
+fig = plt.figure(figsize=[10, 6])
+ax = plt.subplot(111)
+
+ax.set_title('Averaged Profile Mass Spectrum')
+ax.set_xlabel('m/z')
+ax.set_ylabel('Intensity')
+
+
+ax.plot(mzs, ints, linewidth=0.8)
+
+plt.show()
+
+signal, intensity = mzs, ints
+
+# Find indices of the top 10 maximum values in the "signal" array
+top_indices = np.argpartition(intensity, -10)[-10:]
+
+# Select corresponding values from the "intensity" array
+selected_intensity = intensity[top_indices]
+
+# Display the results
+print("Top 10 signal values:", signal[top_indices])
+print("Corresponding intensity values:", selected_intensity)
